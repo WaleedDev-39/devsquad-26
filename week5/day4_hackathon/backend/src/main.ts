5b2b3566eb2effb7d83853e5f53ea827f9f8d582
WaleedDev-39 <m.waleed.a.03@gmail.com>
2026-05-25 21:30:09 +0500

    deployment bug fixed 2

--- week5/day4_hackathon/backend/src/main.ts
+++ week5/day4_hackathon/backend/src/main.ts
@@ -29,6 +29,6 @@ async function bootstrap() {
 
   const port = process.env.PORT || 5000;
   await app.listen(port, '0.0.0.0');
-  console.log(`🚀 Server running on port ${port}`);}
+  console.log(`🚀 Server running on port ${port}`);
 }
 bootstrap();
